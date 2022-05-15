741372d64dd82997deefd9aad54550ad16ed2238
EddieFromIndia <bassistatanu@gmail.com>
2022-05-15 18:46:45 +0530

    Add project files.

---
+++ SD Sound Card Programmer/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SD_Sound_Card_Programmer;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
